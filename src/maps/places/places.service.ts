aa02a2e09188fb0b5a4d9f910cda33c427065611
Gabriel Oliveira Borges <gabriel.oliveiraborges@gmail.com>
2024-12-15 20:10:21 -0300

    Adding google maps api key in .env through @nestjs/config

--- src/maps/places/places.service.ts
+++ src/maps/places/places.service.ts
@@ -3,17 +3,23 @@ import {
   Client as MapsClient,
   PlaceInputType,
 } from '@googlemaps/google-maps-services-js';
+import { ConfigService } from '@nestjs/config';
+import { EnvVarsKeys } from '../../utils/consts';
 
 @Injectable()
 export class PlacesService {
-  constructor(private readonly mapsClient: MapsClient) {}
+  constructor(
+    private readonly mapsClient: MapsClient,
+    private readonly configService: ConfigService,
+  ) {}
+
   async findPlaces(text: string) {
     const { data } = await this.mapsClient.findPlaceFromText({
       params: {
         input: text,
         inputtype: PlaceInputType.textQuery,
         fields: ['place_id', 'formatted_address', 'geometry', 'name'],
-        key: '',
+        key: this.configService.get(EnvVarsKeys.GOOGLE_MAPS_API_KEY),
       },
     });
 
---
+++ src/utils/consts.ts
@@ -0,0 +1,3 @@
+export enum EnvVarsKeys {
+  GOOGLE_MAPS_API_KEY = 'GOOGLE_MAPS_API_KEY',
+}
