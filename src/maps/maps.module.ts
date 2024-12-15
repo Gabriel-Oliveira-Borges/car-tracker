f9fcb537c5f89dc054c1a1d8bb87fd19b5d30ace
Gabriel Oliveira Borges <gabriel.oliveiraborges@gmail.com>
2024-12-14 23:18:19 -0300

    Adding google maps client

--- src/maps/maps.module.ts
+++ src/maps/maps.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PlacesController } from './places/places.controller';
 import { PlacesService } from './places/places.service';
+import { Client as MapsClient } from '@googlemaps/google-maps-services-js';
+
+/**
+ * Configuração do provedor para o MapsClient.
+ *    Como o google maps é uma dependência externa, que não é gerenciada pelo NestJS com os seus decorators, é necessário criar um provider para o MapsClient.
+ * @provide Especifica o token que será usado para injetar a dependência.
+ * @useValue Instancia um novo MapsClient que será fornecido quando o token for solicitado.
+ */
+const mapsProviderConfig = {
+  provide: MapsClient,
+  useValue: new MapsClient(),
+};
 
 @Module({
   imports: [],
   controllers: [PlacesController],
-  providers: [PlacesService],
+  providers: [PlacesService, mapsProviderConfig],
 })
 export class MapsModule {}
--- src/maps/places/places.service.ts
+++ src/maps/places/places.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import {
+  Client as MapsClient,
+  PlaceInputType,
+} from '@googlemaps/google-maps-services-js';
 
 @Injectable()
 export class PlacesService {
-  findPlaces(text: string) {
-    return `Finding places with the text: ${text}`;
+  constructor(private readonly mapsClient: MapsClient) {}
+  async findPlaces(text: string) {
+    const { data } = await this.mapsClient.findPlaceFromText({
+      params: {
+        input: text,
+        inputtype: PlaceInputType.textQuery,
+        fields: ['place_id', 'formatted_address', 'geometry', 'name'],
+        key: '',
+      },
+    });
+
+    return data;
   }
 }
